32341aeef1e0805694d1bce0d4a6f6dfa2009f9e
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2022-09-05 22:02:12 +0200

    Ustawiony pierwszy tekst.

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -12,7 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,7 +48,10 @@ fun TaskCompleted() {
             contentDescription = null,
         )
         Text(
-            text = "Wszystkie zadania ukończone"
+            text = stringResource(R.string.text_1),
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
         )
         Text(
             text = "Dobra robota!"
